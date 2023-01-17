89895a6ab70d1b9c84a4d787e4706164612aeac6
jmollthb <122883523+jmollthb@users.noreply.github.com>
2023-01-17 15:04:44 +0100

    PHP Anwendung

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <b><p style="font-size: 18">Annuitätenkreditrechner</p></b>
+    <form action="abgabe_tilgungsplan.php" method="post">
+        
+        Kredit (in €): <br>
+		<input type="int" name="kredit"/> <br>
+        Zins (in%): <br>
+		<input type="int" name="zins"/> <br>
+        Jährliche Annuität (in €): <br>
+		<input type="int" name="annuitaet"/> <br>
+        <input type="Submit"/>
+    </form>
+    Kredit:  €<br />
+Zinsen:  %<br />
+Annuität:  €<br />
+    
+</body>
+</html>
